0a67d312a33d9434fd89e2e550d4c4e4eea7d950
David First <davidnet@gmail.com>
2024-09-13 14:05:57 -0400

    example is ready

--- pkg1/index.js
+++ pkg1/index.js
@@ -1,17 +1,35 @@
+import fs from 'fs';
+
 async function loadPkg2() {
   try {
     const { num } = await import('../pkg2/index.js');
     console.log("num:", num)
   } catch (err) {
-    console.log('Error loading pkg2', err);
+    console.log('Error loading pkg2', err.message);
   }
 }
 
 async function run() {
+  removeTypeModule();
   await loadPkg2();
-  // wait 10 seconds
-  await new Promise((resolve) => setTimeout(resolve, 8000));
+  addTypeModule();
+  checkTypeModule();
   await loadPkg2();
 }
 
 run();
+
+function addTypeModule() {
+  const pkgJson = JSON.parse(fs.readFileSync('./pkg2/package.json'));
+  pkgJson.type = 'module';
+  fs.writeFileSync('./pkg2/package.json', JSON.stringify(pkgJson, null, 2));
+}
+function removeTypeModule() {
+  const pkgJson = JSON.parse(fs.readFileSync('./pkg2/package.json'));
+  delete pkgJson.type;
+  fs.writeFileSync('./pkg2/package.json', JSON.stringify(pkgJson, null, 2));
+}
+function checkTypeModule() {
+  const pkgJson = JSON.parse(fs.readFileSync('./pkg2/package.json'));
+  console.log('type:', pkgJson.type);
+}
